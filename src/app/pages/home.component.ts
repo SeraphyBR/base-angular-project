1420ce512f3b75b1a32f0b66522dbdb9145ee36a
SeraphyBR <luisjuniorbr@gmail.com>
2024-12-12 07:48:06 -0300

    Adicao de logout

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -1,24 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostBinding } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
 import AuthService from '../services/auth.service';
 
 @Component({
   selector: 'app-home-page',
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   template: `
-    <mat-card class="tw-min-w-80 tw-rounded tw-p-6">
+    <mat-card class="tw-min-w-80 tw-gap-4 tw-rounded tw-p-6">
       <p class="tw-text-xl">Bem Vindo, {{ username }}!</p>
+      <button mat-button (click)="logout()">Logout</button>
     </mat-card>
   `,
 })
 export default class HomePageComponent {
   @HostBinding('class') className = 'tw-bg-blue-400 tw-flex tw-justify-center tw-items-center tw-h-screen';
 
-  username: string;
+  protected username: string;
 
-  constructor(private auth: AuthService) {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) {
     const user = this.auth.getLoggedUser();
     this.username = user ? user.fullname : 'Visitante';
+  }
+
+  protected logout() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
   }
 }
